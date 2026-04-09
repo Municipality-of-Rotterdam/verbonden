c173eff5893e6f5fdbb39936a1072afea553bd52
Kees van Dieren <kees@squins.com>
2026-04-09 22:34:03 +0200

    Wicket test infra

---
+++ src/test/java/nl/rotterdam/huwelijk/features/marriage_intake/ui/MarriageIntakePageTest.java
@@ -0,0 +1,16 @@
+package nl.rotterdam.huwelijk.features.marriage_intake.ui;
+
+import nl.rotterdam.huwelijk.integration_test.BaseWicketTest;
+import nl.rotterdam.huwelijk.integration_test.HuwelijkIntegrationTest;
+import org.junit.jupiter.api.Test;
+
+
+class MarriageIntakePageTest extends BaseWicketTest {
+
+    @Test
+    void testRender() {
+        tester.startPage(MarriageIntakePage.class);
+        tester.assertRenderedPage(MarriageIntakePage.class);
+    }
+
+}
