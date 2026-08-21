b37ce9346a70d3658771a61881d0a4d8e56205da
Kees van Dieren <kees@squins.com>
2026-08-21 12:17:03 +0200

    #67 remove redundant modifier
    Since a while public is no longer needed for main methods.

--- app-local/src/main/java/nl/rotterdam/huwelijk/HuwelijkApplication.java
+++ app-local/src/main/java/nl/rotterdam/huwelijk/HuwelijkApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HuwelijkApplication {
 
-    public static void main(String[] args) {
+    static void main(String[] args) {
         SpringApplication.run(HuwelijkApplication.class, args);
     }
 }
